eca470fdb23dda75af2de45947a4f250dfecfbf5
kuehar <green95251@gmail.com>
2021-06-14 00:23:42 +0900

    Initial Commit

---
+++ Janken-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Janken-app
+//
+//  Created by kuehar on 2021/06/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        
+        Image("gu")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Janken-app/Janken_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Janken_appApp.swift
+//  Janken-app
+//
+//  Created by kuehar on 2021/06/14.
+//
+
+import SwiftUI
+
+@main
+struct Janken_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
